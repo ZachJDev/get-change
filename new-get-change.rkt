0ab2af38478dbbee2f63bfc4e5c681184c5f7ac2
Zach Johnson <Zachjdev2121@gmail.com>
2021-01-26 20:14:15 -0500

    new Scheme get-change; leaner but less descriptive

--- new-get-change.rkt
+++ new-get-change.rkt
@@ -1,25 +1,18 @@
 #lang scheme
 
 (define (get-change n)
+  (if (< n 0)                                        
+        '((quarters 0) (dimes 0) (nickels 0) (pennies 0))
   (let* (
          (val-qs (* 25 (floor (/ n 25))))
-         (val-ds (* 10 (floor (/ (- n val-qs) 10))))
-         (val-ns (*  5 (floor (/ (- n
-                                    val-qs
-                                    val-ds) 5)))))
-    (if (< n 0)
-        '((quarters 0) (dimes 0) (nickels 0) (pennies 0))
+         (remainder-one (- n val-qs))
+         (val-ds (* 10 (floor (/ remainder-one 10))))
+         (remainder-two (- remainder-one val-ds))
+         (val-ns (*  5 (floor (/ remainder-two 5)))))
+    
      (list (list 'quarters (floor (/ val-qs 25)))
-           (list 'dimes (floor
-                         (/
-                          (- n  val-qs) 10)))
-           (list' nickels (floor
-                          (/
-                           (- n val-qs val-ds) 5)))
-           (list 'pennies (floor
-                           (/
-                            (- n val-qs val-ds val-ns)
-                            1))))
-      )))
+           (list 'dimes (floor (/ remainder-one 10)))
+           (list 'nickels (floor (/ remainder-two 5)))
+           (list 'pennies (floor (/ (- remainder-two val-ns) 1)))))))
                              
 (get-change 96) 
